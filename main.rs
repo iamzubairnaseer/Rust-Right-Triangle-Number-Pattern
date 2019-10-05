2fa692e814ed50f60283cb6a4abb90bedf9114ca
Zubair Naseer <40863362+iamzubairnaseer@users.noreply.github.com>
2019-10-05 15:51:51 +0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,7 +1,7 @@
 fn main(){
       for i in 1..6{//here you can change 6 to any number you want.
         for j in 1..=i{
-            print!("i");
+            print!("{}",i);
         }
         println!("");
       }
